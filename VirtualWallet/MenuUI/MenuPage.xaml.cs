b8f313680b2e855b23b1032b65eced25a0637ba2
Lukas Valach <luvalach@seznam.cz>
2016-04-18 23:14:57 +0200

    UI: MenuPage added

---
+++ VirtualWallet/MenuUI/MenuPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using BL.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VirtualWallet.ViewModels;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace VirtualWallet
+{
+    public sealed partial class MenuPage : Page
+    {
+        private MenuPageViewModel viewModel;
+
+        public MenuPage()
+        {
+            this.InitializeComponent();
+            viewModel = new MenuPageViewModel(new WalletService(), new BankService(), new CategoryService());
+            this.DataContext = viewModel;
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            await viewModel.LoadData();
+            base.OnNavigatedTo(e);
+        }
+
+        private void GridViewWallets_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            //WalletViewModel s = e.ClickedItem as WalletViewModel;
+            //this.Frame.Navigate(typeof(WalletDetailPage), s);
+        }
+
+        private void GridViewBanks_ItemClick(object sender, ItemClickEventArgs e)
+        {
+
+        }
+
+        private void GridViewCategories_ItemClick(object sender, ItemClickEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ VirtualWallet/ViewModels/MenuPageViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BL.Service;
+
+namespace VirtualWallet.ViewModels
+{
+    class MenuPageViewModel : ViewModelBase
+    {
+        private BankService bankService;
+        private CategoryService categoryService;
+        private WalletService walletService;
+
+        public MenuPageViewModel(WalletService walletService, BankService bankService, CategoryService categoryService)
+        {
+            this.walletService = walletService;
+            this.bankService = bankService;
+            this.categoryService = categoryService;
+        }
+
+        public async Task LoadData()
+        {
+            
+        }
+    }
+}
