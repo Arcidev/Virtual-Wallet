62efe4000076cb2334279518289fea6996f191f7
Arci <Arcidev@users.noreply.github.com>
2016-04-21 00:55:44 +0200

    UI: added some content into settings page related to deleting stored data

--- VirtualWallet/Pages/Settings/SettingsPage.xaml.cs
+++ VirtualWallet/Pages/Settings/SettingsPage.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using BL.Service;
+using System;
 using VirtualWallet.ViewModels;
+using Windows.UI.Popups;
 using Windows.UI.Xaml.Controls;
 
 namespace VirtualWallet.Pages
@@ -12,6 +14,25 @@ namespace VirtualWallet.Pages
         {
             this.InitializeComponent();
             viewModel = new SettingsViewModel(new DatabaseService(), new BankService());
+        }
+
+        private void RemoveCredentialsButton_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            ShowRemoveDataDialog("all credentials", viewModel.RemoveAllCredentialsCommand.Execute);
+        }
+
+        private void RemoveAllDataButton_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            ShowRemoveDataDialog("all data and credentials", viewModel.RemoveAllDataCommand.Execute);
+        }
+
+        private async void ShowRemoveDataDialog(string dataToRemove, UICommandInvokedHandler yesHandler)
+        {
+            var dialog = new MessageDialog($"By performing this action you will erase {dataToRemove} stored by this application. Are you sure you want to continue?");
+            dialog.Commands.Add(new UICommand("Yes", yesHandler));
+            dialog.Commands.Add(new UICommand("No"));
+
+            await dialog.ShowAsync();
         }
     }
 }
