ef22c3bc345e0635c252abc1a49589aa7a1f1bde
Lukas Valach <luvalach@seznam.cz>
2016-04-29 16:17:44 +0200

    DAL: Initial data will be deleted and reloaded during application startup.

--- DAL/InitialData/InitialDataDev.cs
+++ DAL/InitialData/InitialDataDev.cs
@@ -13,46 +13,36 @@ namespace DAL.InitialData
         // Just for testing
         public static async Task InitTempData(SQLiteAsyncConnection connection)
         {
-            // Prevent this to multiply on every start of app
-            if (!(await connection.Table<Wallet>().ToListAsync()).Any())
+            await connection.DeleteAllAsync<CategoryRule>();
+            await connection.DeleteAllAsync<Rule>();
+            await connection.DeleteAllAsync<WalletCategory>();
+            await connection.DeleteAllAsync<Category>();
+            await connection.DeleteAllAsync<Wallet>();
+
+            await connection.InsertAllAsync(new object[]
             {
-                await connection.InsertAllAsync(new object[]
-                {
                     new Wallet() { Id = 1,  Name = "Test wallet", ImageId = 15 }
-                });
-            }
+            });
 
-            if (!(await connection.Table<Category>().ToListAsync()).Any())
+            await connection.InsertAllAsync(new object[]
             {
-                await connection.InsertAllAsync(new object[]
-                {
                     new Category() { Id = 1, Name = "Test category", ImageId = 139 }
-                });
-            }
+            });
 
-            if (!(await connection.Table<WalletCategory>().ToListAsync()).Any())
+            await connection.InsertAllAsync(new object[]
             {
-                await connection.InsertAllAsync(new object[]
-                {
                     new WalletCategory() { WalletId = 1, CategoryId = 1 }
-                });
-            }
+            });
 
-            if (!(await connection.Table<Rule>().ToListAsync()).Any())
+            await connection.InsertAllAsync(new object[]
             {
-                await connection.InsertAllAsync(new object[]
-                {
                     new Rule() { Id = 1, Name = "Test rule", Description = "This is test rule", Pattern = "*" }
-                });
-            }
+            });
 
-            if (!(await connection.Table<CategoryRule>().ToListAsync()).Any())
+            await connection.InsertAllAsync(new object[]
             {
-                await connection.InsertAllAsync(new object[]
-                {
                     new CategoryRule() { RuleId = 1, CategoryId = 1 }
-                });
-            }
+            });
         }
     }
 }
