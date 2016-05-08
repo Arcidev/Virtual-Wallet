fab3ef3ef9f1552c5b069937b23d627dbc58751a
Lukas Valach <luvalach@seznam.cz>
2016-05-08 15:30:00 +0200

    BL: Equals method of Image model overridden.

--- BL/Models/Image.cs
+++ BL/Models/Image.cs
@@ -6,5 +6,25 @@ namespace BL.Models
         public int Id { get; set; }
 
         public string Path { get; set; }
+
+        public override bool Equals(System.Object obj)
+        {
+            // If parameter is null return false.
+            if (obj == null)
+            {
+                return false;
+            }
+
+            // If parameter cannot be cast to Point return false.
+            Image i = obj as Image;
+            if (i == null)
+            {
+                return false;
+            }
+
+            // Return true if the fields match:
+            return (this.Id == i.Id) && (this.Path.Equals(i.Path));
+        }
+
     }
 }
