4541396a523db806a8514a83866a5b80949cc4a5
Arci <Arcidev@users.noreply.github.com>
2016-05-08 16:04:13 +0200

    Misc: fixed warning and Spanish translation

--- BL/Models/Image.cs
+++ BL/Models/Image.cs
@@ -7,7 +7,7 @@ namespace BL.Models
 
         public string Path { get; set; }
 
-        public override bool Equals(System.Object obj)
+        public override bool Equals(object obj)
         {
             // If parameter is null return false.
             if (obj == null)
@@ -15,7 +15,7 @@ namespace BL.Models
                 return false;
             }
 
-            // If parameter cannot be cast to Point return false.
+            // If parameter cannot be cast to Image return false.
             Image i = obj as Image;
             if (i == null)
             {
@@ -26,5 +26,12 @@ namespace BL.Models
             return (this.Id == i.Id) && (this.Path.Equals(i.Path));
         }
 
+        public override int GetHashCode()
+        {
+            int hash = 13;
+            hash = (hash * 7) + Id.GetHashCode();
+            hash = (hash * 7) + Path?.GetHashCode() ?? 0;
+            return hash;
+        }
     }
 }
