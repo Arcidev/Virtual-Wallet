ee42a4ccfa0e77b4c39a7229cf5a885f7824acf8
Arci <Arcidev@users.noreply.github.com>
2016-05-08 17:08:00 +0200

    Misc: added public class modifiers

--- VirtualWallet/Converters/BoolToVisibilityConverter.cs
+++ VirtualWallet/Converters/BoolToVisibilityConverter.cs
@@ -4,7 +4,7 @@ using Windows.UI.Xaml.Data;
 
 namespace VirtualWallet.Converters
 {
-    class BoolToVisibilityConverter : IValueConverter
+    public class BoolToVisibilityConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, string language)
         {
