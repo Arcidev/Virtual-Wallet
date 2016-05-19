6d912d0586d618300f6c550d79a518463191f09b
Lukas Valach <luvalach@seznam.cz>
2016-05-19 23:05:04 +0200

    UI: WalletsPage added.

---
+++ VirtualWallet/Pages/Wallet/WalletsPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using BL.Models;
+using BL.Service;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using VirtualWallet.ViewModels;
+using Windows.ApplicationModel.Resources;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace VirtualWallet.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class WalletsPage : Page
+    {
+        private WalletsPageViewModel viewModel;
+        private ResourceLoader resources;
+
+        public WalletsPage()
+        {
+            resources = ResourceLoader.GetForCurrentView();
+            this.InitializeComponent();
+            viewModel = new WalletsPageViewModel(new WalletService());
+            this.DataContext = viewModel;
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {            
+            await viewModel.LoadDataAsync();
+            base.OnNavigatedTo(e);
+        }
+
+        private void AddAppBarButton_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            var pagePayload = new PagePayload() { Dto = new Wallet() { Name = resources.GetString("Wallet_WalletDefaultName") } };
+            Frame.Navigate(typeof(WalletPage), pagePayload);
+        }
+
+        private void GridViewWallets_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            var wallet = (Wallet)e.ClickedItem; ;
+            var walletPageDto = new BL.Models.PagePayload() { Dto = wallet };
+            Frame.Navigate(typeof(WalletPage), walletPageDto);
+        }
+    }
+}
---
+++ VirtualWallet/ViewModels/Wallet/WalletsPageViewModel.cs
@@ -0,0 +1,42 @@
+﻿using BL.Models;
+using BL.Service;
+using Shared.Filters;
+using Shared.Modifiers;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+
+namespace VirtualWallet.ViewModels
+{
+    class WalletsPageViewModel : ViewModelBase
+    {
+        private IWalletService walletService;
+        
+        private ObservableCollection<Wallet> wallets;
+
+        public WalletsPageViewModel(IWalletService walletService)
+        {
+            this.walletService = walletService;
+        }
+
+        public ObservableCollection<Wallet> Wallets
+        {
+            get { return wallets; }
+            set
+            {
+                if (wallets == value)
+                    return;
+
+                wallets = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public async Task LoadDataAsync()
+        {
+            var walletModifier = new WalletModifier() { IncludeImage = true };
+            var wallets = await walletService.GetAllAsync(walletModifier);
+            Wallets = new ObservableCollection<Wallet>(wallets);
+        }
+    }
+}
